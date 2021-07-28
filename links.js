f0bcf68fb41d34afeff103ca578caccdcbc7c4de
Roman Ožana <roman@ozana.cz>
2021-07-28 15:42:01 +0200

    Add more

--- links.js
+++ links.js
@@ -1,40 +1,43 @@
 export const links = {
 	// ozana.cz
-	"ozana.cz/homepage" : "https://ozana.cz",
-	"ozana.cz/work" : "https://ozana.cz/work",
-	"ozana.cz/resume" : "https://ozana.cz/resume",
+	"ozana.cz/homepage": "https://ozana.cz",
+	"ozana.cz/work": "https://ozana.cz/work",
+	"ozana.cz/resume": "https://ozana.cz/resume",
 
 	// ozzyczech.cz
 
 	"ozzyczech.cz/homepage": "https://ozzyczech.cz/",
 
 	// testomato.com
-	"testomato.com/homepage" : "https://www.testomato.com",
-	"testomato.com/pricing" : "https://www.testomato.com/pricing",
-	"testomato.com/contact" : "https://www.testomato.com/contact",
-	"testomato.com/bot" : "https://www.testomato.com/bot",
-	"blog.testomato.com/homepage" : "https://blog.testomato.com",
-	"help.testomato.com/homepage" : "https://help.testomato.com",
+	"testomato.com/homepage": "https://www.testomato.com",
+	"testomato.com/pricing": "https://www.testomato.com/pricing",
+	"testomato.com/contact": "https://www.testomato.com/contact",
+	"testomato.com/bot": "https://www.testomato.com/bot",
+	"blog.testomato.com/homepage": "https://blog.testomato.com",
+	"help.testomato.com/homepage": "https://help.testomato.com",
 
 	// zdrojak.cz
-	"zdrojak.cz/homepage" : "https://www.zdrojak.cz",
-	"zdrojak.cz/webdesign" : "https://www.zdrojak.cz/webdesign/",
-	"zdrojak.cz/autori-roman-ozana" : "https://zdrojak.cz/autori/roman-ozana/",
+	"zdrojak.cz/homepage": "https://www.zdrojak.cz",
+	"zdrojak.cz/webdesign": "https://www.zdrojak.cz/webdesign/",
+	"zdrojak.cz/autori-roman-ozana": "https://zdrojak.cz/autori/roman-ozana/",
 
 	// https://sphido.org/
-	"sphido.org/homepage" : "https://sphido.org/",
-	"sphido.org/packages-sphido-core" : "https://sphido.org/packages/sphido-core",
-	"sphido.org/packages-sphido-emoji" : "https://sphido.org/packages/sphido-emoji",
+	"sphido.org/homepage": "https://sphido.org/",
+	"sphido.org/packages-sphido-core": "https://sphido.org/packages/sphido-core",
+	"sphido.org/packages-sphido-emoji": "https://sphido.org/packages/sphido-emoji",
+
+	// tw2pic.com
+	"tw2pic.com/homepage": "https://tw2pic.com/",
 
 	// wikidi.cz
-	"wikidi.cz/homepage" : "https://www.wikidi.cz/",
-
-	// zuri.com
-	"zuri.cz/homepage" : "https://www.zuri.com/",
+	"wikidi.cz/homepage": "https://www.wikidi.cz/",
 
 	// others
-	"pozarniaznalecka.cz/homepage" : "http://www.pozarniaznalecka.cz/",
-	"lagunanj.cz/homepage" : "https://lagunanj.cz/",
-	"mluvimspanelsky.cz/homepage" : "http://mluvimspanelsky.cz/",
-	"sazenice-stromky.cz/homepage" : "https://www.sazenice-stromky.cz/",
+	"pozarniaznalecka.cz/homepage": "http://www.pozarniaznalecka.cz/",
+	"lagunanj.cz/homepage": "https://lagunanj.cz/",
+	"mluvimspanelsky.cz/homepage": "http://mluvimspanelsky.cz/",
+	"sazenice-stromky.cz/homepage": "https://www.sazenice-stromky.cz/",
+	"zuboz.cz/homepage": "http://www.zuboz.cz/",
+	"portalbozp.cz/homepage": "http://www.portalbozp.cz/",
+	"ergowork.cz/homepage": "http://www.ergowork.cz/",
 }
